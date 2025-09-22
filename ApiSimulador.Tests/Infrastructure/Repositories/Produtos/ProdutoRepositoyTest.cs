2136a74bc7bf9a768cab7bfc94495e36098d7d19
georgelucasvieira <george.vieira.carmo@gmail.com>
2025-09-22 14:40:45 -0300

    setup de testes

---
+++ ApiSimulador.Tests/Infrastructure/Repositories/Produtos/ProdutoRepositoyTest.cs
@@ -0,0 +1,8 @@
+﻿using ApiSimulador.Infrastructure.Data.Contexts;
+
+namespace ApiSimulador.Tests.Infrastructure.Repositories.Produtos;
+
+public class ProdutoRepositoyTest
+{
+    private readonly ApplicationDbContext _dbContext;
+}

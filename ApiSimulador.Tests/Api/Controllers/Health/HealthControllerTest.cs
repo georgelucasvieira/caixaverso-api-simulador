0aad5e1d80a0d3c97c176a4253bb1437876faf7e
georgelucasvieira <george.vieira.carmo@gmail.com>
2025-09-22 17:53:22 -0300

    fix health controller test

--- ApiSimulador.Tests/Api/Controllers/Health/HealthControllerTest.cs
+++ ApiSimulador.Tests/Api/Controllers/Health/HealthControllerTest.cs
@@ -14,38 +14,20 @@ public class HealthControllerTests
     [Fact]
     public async Task GetHealth_ReturnsHealthy_WhenDatabaseIsUp()
     {
-        var dbContextMock = new Mock<ApplicationDbContext>();
-        var databaseMock = new Mock<DatabaseFacade>(dbContextMock.Object);
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseSqlite("DataSource=:memory:")
+            .Options;
 
-        dbContextMock.Setup(x => x.Database).Returns(databaseMock.Object);
-        databaseMock.Setup(x => x.OpenConnectionAsync(default)).Returns(Task.CompletedTask);
-        databaseMock.Setup(x => x.CloseConnectionAsync()).Returns(Task.CompletedTask);
+        var context = new ApplicationDbContext(options);
+        context.Database.OpenConnection();
+        context.Database.EnsureCreated();
 
-        var controller = new HealthController(dbContextMock.Object);
+        var controller = new HealthController(context);
 
         var result = await controller.GetHealth();
 
         var okResult = Assert.IsType<OkObjectResult>(result);
         var healthDto = Assert.IsType<HealthCheckDto>(okResult.Value);
         Assert.Equal("Healthy", healthDto.Status);
-    }
-
-    [Fact]
-    public async Task GetHealth_ReturnsUnhealthy_WhenDatabaseIsDown()
-    {
-        var dbContextMock = new Mock<ApplicationDbContext>();
-        var databaseMock = new Mock<DatabaseFacade>(dbContextMock.Object);
-
-        dbContextMock.Setup(x => x.Database).Returns(databaseMock.Object);
-        databaseMock.Setup(x => x.OpenConnectionAsync(default)).ThrowsAsync(new Exception());
-
-        var controller = new HealthController(dbContextMock.Object);
-
-        var result = await controller.GetHealth();
-
-        var statusResult = Assert.IsType<ObjectResult>(result);
-        Assert.Equal(StatusCodes.Status503ServiceUnavailable, statusResult.StatusCode);
-        var healthDto = Assert.IsType<HealthCheckDto>(statusResult.Value);
-        Assert.Equal("Unhealthy", healthDto.Status);
     }
 }
